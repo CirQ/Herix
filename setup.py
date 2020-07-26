7b602b92cc24af79be21d41a14536d7ac6a44439
WANG Sinan <wsnansw@gmail.com>
2020-07-26 21:41:05 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name='Herix',
+      version='1.0',
+      description='HelpER for Information eXtraction and generation from web',
+      author='CirQ',
+      author_email='wsnansw@gmail.com',
+      url='https://github.com/CirQ/Herix/',
+      scripts=['ui.py'],
+)
